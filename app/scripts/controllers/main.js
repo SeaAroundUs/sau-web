1cc0075ebb961c2a5a70eac291ec9fc6059a5536
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-01-27 19:54:20 -0800

    integrated leaflet, bound footer to bottom

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,23 +8,63 @@
  * Controller of the sauWebApp
  */
 angular.module('sauWebApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$scope', '$http', 'leafletData', function ($scope, $http, leafletData) {
+
+    leafletData.getMap('mainmap').then(function(map) {
+      window.map = map;
+    });
+
+    angular.extend($scope, {
+
+      center: {
+        lat: 0,
+        lng: 0,
+        zoom: 3
+      },
+      defaults: {
+        tileLayer: 'http://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png',
+        tileLayerOptions: {
+          continuousWorld: false,
+          noWrap: true,
+          detectRetina: true, // no idea what this does
+          reuseTiles: true // nor this
+        },
+
+      }
+    });
+
+    $scope.searchIP = function(ip) {
+      var url = "http://freegeoip.net/json/" + ip;
+      $http.get(url).success(function(res) {
+        console.log(res);
+        $scope.center = {
+          lat: res.latitude,
+          lng: res.longitude,
+          zoom: 3
+        }
+        $scope.ip = res.ip;
+        console.log(res);
+      });
+    };
+
+    $scope.searchIP("");
+
     var templateDir = 'views/';
     $scope.templates = [
-        {'name': 'Analyses and Visualization', 'url': templateDir+'data.html'},
-        {'name': 'News', 'url': templateDir+'news.html'},
-        {'name': 'Publications', 'url': templateDir+'publications.html'},
-        {'name': 'People', 'url': templateDir+'people.html'},
-        {'name': 'Collaborations', 'url': templateDir+'collaborations.html'},
-        {'name': 'About Us', 'url': templateDir+'about.html'},
+      {'name': 'Analyses and Visualization', 'url': templateDir+'data.html'},
+      {'name': 'News', 'url': templateDir+'news.html'},
+      {'name': 'Publications', 'url': templateDir+'publications.html'},
+      {'name': 'People', 'url': templateDir+'people.html'},
+      {'name': 'Collaborations', 'url': templateDir+'collaborations.html'},
+      {'name': 'About Us', 'url': templateDir+'about.html'},
     ];
     $scope.template = $scope.templates[0];
 
     $scope.log = function(txt) {
-        console.log(txt);
+      console.log(txt);
     };
 
     $scope.updateInclude = function(t) {
-        $scope.template = t;
+      $scope.template = t;
     };
-  });
+  }]);
