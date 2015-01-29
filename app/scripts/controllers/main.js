5aa507ba162f00382a52e73e194a42c65087e4a4
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-01-28 18:06:42 -0800

    refactor: extracted MapCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,91 +8,7 @@
  * Controller of the sauWebApp
  */
 angular.module('sauWebApp')
-  .controller('MainCtrl', ['$scope', '$http', 'leafletData', function ($scope, $http, leafletData) {
-
-    $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, feature, leafletEvent) {
-        var layer = leafletEvent.target;
-        layer.setStyle(highlightStyle);
-        layer.bringToFront();
-        $scope.hoverRegion = feature;
-    });
-
-    $scope.$on("leafletDirectiveMap.geojsonMouseout", function(ev, feature, leafletEvent) {
-        $scope.hoverRegion = null;
-    });
-
-    $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
-        regionClick(featureSelected, leafletEvent);
-    });
-
-
-    var highlightStyle = {
-        fillColor: '#00f',
-    };
-
-    var defaultStyle = {
-      color: 'black',
-      stroke: true,
-      weight: 1,
-      opacity: 1.0,
-      fillColor: 'black',
-      fillOpacity: 0.3,
-      lineCap: 'round'
-    };
-
-    var maxBounds = [
-      [-89, -180],
-      [89, 180]
-    ];
-
-    angular.extend($scope, {
-
-      center: {
-        lat: 0,
-        lng: 0,
-        zoom: 3
-      },
-
-      defaults: {
-        maxBounds: maxBounds,
-        tileLayer: 'http://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png',
-        tileLayerOptions: {
-          noWrap: true,
-          detectRetina: true, // no idea what this does
-          reuseTiles: true // nor this
-        },
-
-      }
-    });
-
-    // get regions
-    var url = 'http://localhost:8000/api/v1/eez/';
-    $http.get(url)
-      .success(function(data, status) {
-        angular.extend($scope, {
-          geojson: {
-            data: data.data,
-            style: defaultStyle,
-            resetStyleOnMouseout: true
-          }
-        });
-      });
-
-    $scope.searchIP = function(ip) {
-      var url = 'http://freegeoip.net/json/' + ip;
-      $http.defaults.headers.common = {};
-      $http.get(url).success(function(res) {
-        $scope.center = {
-          lat: res.latitude,
-          lng: res.longitude,
-          zoom: 3
-        };
-        $scope.ip = res.ip;
-        console.log(res);
-      });
-    };
-
-    // $scope.searchIP('');
+  .controller('MainCtrl', ['$scope', function ($scope) {
 
     var templateDir = 'views/';
     $scope.templates = [
@@ -104,6 +20,7 @@ angular.module('sauWebApp')
       {'name': 'About Us', 'url': templateDir+'about.html'},
     ];
     $scope.template = $scope.templates[0];
+    $scope.hoverRegion = {};
 
     $scope.log = function(txt) {
       console.log(txt);
---
+++ app/scripts/controllers/map.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sauWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sauWebApp
+ */
+angular.module('sauWebApp')
+  .controller('MapCtrl', ['$scope', '$http', 'leafletData', function ($scope, $http, leafletData) {
+
+    $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, feature, leafletEvent) {
+        var layer = leafletEvent.target;
+        layer.setStyle(highlightStyle);
+        layer.bringToFront();
+        $scope.$parent.$parent.hoverRegion = feature;
+    });
+
+    $scope.$on("leafletDirectiveMap.geojsonMouseout", function(ev, feature, leafletEvent) {
+        $scope.$parent.$parent.hoverRegion = {};
+    });
+
+    $scope.$on("leafletDirectiveMap.geojsonClick", function(ev, featureSelected, leafletEvent) {
+        // regionClick(featureSelected, leafletEvent);
+    });
+
+
+    var highlightStyle = {
+        fillColor: '#00f',
+    };
+
+    var defaultStyle = {
+      color: 'black',
+      stroke: true,
+      weight: 1,
+      opacity: 1.0,
+      fillColor: 'black',
+      fillOpacity: 0.3,
+      lineCap: 'round'
+    };
+
+    var maxBounds = [
+      [-89, -180],
+      [89, 180]
+    ];
+
+    angular.extend($scope, {
+
+      center: {
+        lat: 0,
+        lng: 0,
+        zoom: 3
+      },
+
+      defaults: {
+        maxBounds: maxBounds,
+        tileLayer: 'http://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png',
+        tileLayerOptions: {
+          noWrap: true,
+          detectRetina: true, // no idea what this does
+          reuseTiles: true // nor this
+        },
+      }
+    });
+
+    // get regions
+    var url = 'http://localhost:8000/api/v1/eez/';
+    $http.get(url, {cache: true})
+      .success(function(data, status) {
+        angular.extend($scope, {
+          geojson: {
+            data: data.data,
+            style: defaultStyle,
+            resetStyleOnMouseout: true
+          }
+        });
+      });
+
+    $scope.searchIP = function(ip) {
+      var url = 'http://freegeoip.net/json/' + ip;
+      $http.defaults.headers.common = {};
+      $http.get(url).success(function(res) {
+        $scope.center = {
+          lat: res.latitude,
+          lng: res.longitude,
+          zoom: 3
+        };
+        $scope.ip = res.ip;
+        console.log(res);
+      });
+    };
+
+    // $scope.searchIP('');
+  }]);
