087f0d688063873085effd2868106586e176039a
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-02-03 20:23:40 -0800

    refreshed data on dropdown selection, added all options

--- app/scripts/controllers/region-detail.js
+++ app/scripts/controllers/region-detail.js
@@ -6,6 +6,11 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
     $scope.dimensions = [
       {label: 'taxon', value: 'taxon'},
       {label: 'commercial group', value: 'commercialgroup'},
+      {label: 'functional group', value: 'functionalgroup'},
+      {label: 'country', value: 'country'},
+      {label: 'gear', value: 'gear'},
+      {label: 'sector', value: 'sector'},
+      {label: 'catchtype', value: 'catchtype'},
     ];
 
     $scope.measures = [
@@ -14,23 +19,18 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
     ];
 
     $scope.limits = [
+      {label: '50', value: '50'},
+      {label: '20', value: '20'},
       {label: '10', value: '10'},
       {label: '5', value: '5'},
       {label: '1', value: '1'},
     ];
 
-
     $scope.dimension = $scope.dimensions[0];
     $scope.measure = $scope.measures[0];
-    $scope.limit = $scope.limits[0];
+    $scope.limit = $scope.limits[2];
 
     $scope.feature = sauService.Region.get(options);
-    var data_options = options;
-    data_options.dimension = $scope.dimension.value;
-    data_options.measure = $scope.measure.value;
-    data_options.limit = $scope.limit.value;
-
-    $scope.data = sauService.Data.get(data_options);
 
     $scope.ok = function () {
       $modalInstance.close($scope.feature);
@@ -42,5 +42,16 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
       // FIXME: the request is working fine, but how to get the
       // file downloaded?
       sauService.CSVData.get(data_options);
+    };
+
+    $scope.updateData = function() {
+      console.log("UPDATE");
+      var data_options = options;
+      data_options.dimension = $scope.dimension.value;
+      data_options.measure = $scope.measure.value;
+      data_options.limit = $scope.limit.value;
+      $scope.data = sauService.Data.get(data_options);
     }
+
+    $scope.updateData();
 });
