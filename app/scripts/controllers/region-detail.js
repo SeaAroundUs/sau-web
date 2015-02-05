2ade37f38f11c3f5fee0a135ce7e94c47df36a0d
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-02-04 19:53:56 -0800

    adapted to API data format change, chart improvement, color selector

--- app/scripts/controllers/region-detail.js
+++ app/scripts/controllers/region-detail.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /* global d3 */ /* for jshint */
+/* global colorbrewer */ /* for jshint */
 
 angular.module('sauWebApp').controller('RegionDetailCtrl',
   function ($scope, $modalInstance, $location, $window, sauService, options) {
@@ -21,16 +22,18 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
     ];
 
     $scope.limits = [
-      {label: '50', value: '50'},
       {label: '20', value: '20'},
       {label: '10', value: '10'},
       {label: '5', value: '5'},
       {label: '1', value: '1'},
     ];
 
+    $scope.colors = colorbrewer;
+    $scope.color = $scope.colors.Set1;
+
     $scope.dimension = $scope.dimensions[0];
     $scope.measure = $scope.measures[0];
-    $scope.limit = $scope.limits[2];
+    $scope.limit = $scope.limits[1];
 
     $scope.feature = sauService.Region.get(options);
 
@@ -43,12 +46,20 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
     };
 
     $scope.download = function() {
-      // construct url manually, I don't know how to get it out of the $resource
-      var url = sauService.api_url + options.region + '/'
-        + $scope.measure.value + '/'
-        + $scope.dimension.value
-        + '/?format=csv&limit=' + $scope.limit.value
-        + '&region_id=' + options.region_id;
+      // FIXME: constructing url manually, I don't know how to get it out of the $resource
+      // This should probably be in a service or something too.
+      var url = ['',
+        sauService.api_url,
+        options.region,
+        '/',
+        $scope.measure.value,
+        '/',
+        $scope.dimension.value,
+        '/?format=csv&limit=',
+        $scope.limit.value,
+        '&region_id=',
+        options.region_id,
+      ].join('');
       $window.open(url, '_blank');
     };
 
@@ -58,36 +69,23 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
       data_options.limit = $scope.limit.value;
       data_options.geo_id = options.region_id;
       var data = sauService.Data.get(data_options, function() {
-
-         var keys = d3.keys(data.data);
-         var color = d3.scale.ordinal()
-         .range(['#f00','#0f0', '#00f', '#ff0', '#f0f', '#0ff', '#000',
-           '#08f', '#0f8', '#f80', '#8f0', '#80f', '#f08'
-           ]);
-
-         color.domain(keys);
-
-         var groups = color.domain().map(function (label) {
-           var years = d3.keys(data.data[label]);
-
-           return {
-             key: label,
-             values: years.map(function (year) {
-               return [year, +data.data[label][year]/1000];
-             })
-           };
-         });
-         $scope.data = groups;
+         $scope.data = data.data;
        });
     };
 
-    $scope.updateData();
+
+    $scope.updateColor = function() {
+      if ($scope.color[11]){
+        $scope.options.chart.color = $scope.color[11];
+      } else {
+        $scope.options.chart.color = $scope.color[9];
+      }
+    };
 
     $scope.options = {
         chart: {
             type: 'stackedAreaChart',
             height: 450,
-            width: '100%',
             margin : {
                 top: 20,
                 right: 20,
@@ -97,7 +95,6 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
             x: function(d){return d[0];},
             y: function(d){return d[1];},
             useVoronoi: false,
-            // clipEdge: true,
             transitionDuration: 500,
             useInteractiveGuideline: true,
             xAxis: {
@@ -105,7 +102,6 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
                 tickValues: [1950,1960,1970,1980,1990,2000],
                 tickFormat: function(d) {
                     return d;
-                    // return d3.time.format('%x')(new Date(d))
                 }
             },
             yAxis: {
@@ -116,4 +112,8 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
 
         }
       };
+
+    $scope.updateData();
+    $scope.updateColor();
+
 });
