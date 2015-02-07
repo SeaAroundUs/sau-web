19c75220969f9872990834f0354a219fa43088b2
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-02-06 16:55:18 -0800

    extracted chart code from region-detail to catch-chart

---
+++ app/scripts/controllers/catch-chart.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* global d3 */ /* for jshint */
+/* global colorbrewer */ /* for jshint */
+
+angular.module('sauWebApp').controller('CatchChartCtrl',
+  function ($scope) {
+
+    $scope.options = {
+      chart: {
+          type: 'stackedAreaChart',
+          height: 400,
+          margin : {
+              top: 20,
+              right: 20,
+              bottom: 60,
+              left: 55
+          },
+          x: function(d){return d[0];},
+          y: function(d){return d[1];},
+          transitionDuration: 250,
+          useInteractiveGuideline: true,
+          xAxis: {
+              showMaxMin: false,
+              tickValues: [1950,1960,1970,1980,1990,2000,2010,2020],
+          },
+          yAxis: {
+            tickFormat: function(d){
+              return d3.format(',.1s')(d);
+            }
+          }
+        }
+      };
+
+    $scope.colors = colorbrewer;
+
+    $scope.colors.SAU= {
+      11: ['#f00','#0f0', '#00f', '#ff0', '#f0f', '#0ff', '#000',
+            '#08f', '#0f8', '#f80', '#8f0', '#80f', '#f08'
+          ]
+    };
+
+    $scope.color = $scope.colors.SAU;
+
+    $scope.updateColor = function() {
+      if ($scope.color[11]){
+        $scope.options.chart.color = $scope.color[11];
+      } else {
+        $scope.options.chart.color = $scope.color[9];
+      }
+    };
+
+    $scope.$watch('color', $scope.updateColor);
+
+  });
--- app/scripts/controllers/region-detail.js
+++ app/scripts/controllers/region-detail.js
@@ -1,8 +1,5 @@
 'use strict';
 
-/* global d3 */ /* for jshint */
-/* global colorbrewer */ /* for jshint */
-
 angular.module('sauWebApp').controller('RegionDetailCtrl',
   function ($scope, $modalInstance, $location, $window, sauService, region_options) {
 
@@ -27,16 +24,6 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
       {label: '5', value: '5'},
       {label: '1', value: '1'},
     ];
-
-    $scope.colors = colorbrewer;
-
-    $scope.colors.SAU= {
-      11: ['#f00','#0f0', '#00f', '#ff0', '#f0f', '#0ff', '#000',
-            '#08f', '#0f8', '#f80', '#8f0', '#80f', '#f08'
-          ]
-    };
-
-    $scope.color = $scope.colors.SAU;
 
     $scope.dimension = $scope.dimensions[0];
     $scope.measure = $scope.measures[0];
@@ -77,41 +64,6 @@ angular.module('sauWebApp').controller('RegionDetailCtrl',
        });
     };
 
-    $scope.updateColor = function() {
-      if ($scope.color[11]){
-        $scope.options.chart.color = $scope.color[11];
-      } else {
-        $scope.options.chart.color = $scope.color[9];
-      }
-    };
-
-    $scope.options = {
-      chart: {
-          type: 'stackedAreaChart',
-          height: 450,
-          margin : {
-              top: 20,
-              right: 20,
-              bottom: 60,
-              left: 55
-          },
-          x: function(d){return d[0];},
-          y: function(d){return d[1];},
-          transitionDuration: 250,
-          useInteractiveGuideline: true,
-          xAxis: {
-              showMaxMin: false,
-              tickValues: [1950,1960,1970,1980,1990,2000,2010,2020],
-          },
-          yAxis: {
-            tickFormat: function(d){
-              return d3.format(',.1s')(d);
-            }
-          }
-        }
-      };
-
     $scope.updateData();
-    $scope.updateColor();
 
 });
