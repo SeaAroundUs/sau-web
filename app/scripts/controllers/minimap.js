adc62c358cbfcb871a6756e2844ab2d8b3a114d1
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-02-07 14:11:22 -0800

    culled unnecessary minimap code

--- app/scripts/controllers/minimap.js
+++ app/scripts/controllers/minimap.js
@@ -6,27 +6,19 @@ angular.module('sauWebApp')
   .controller('MiniMapCtrl', function ($scope, sauService, leafletBoundsHelpers, leafletData) {
 
     angular.extend($scope, {
-
       defaults: sauService.mapConfig.defaults,
-      center: {
-        lat: 0,
-        lng: 0,
-        zoom: 3
-      },
-
     });
+
     $scope.feature.$promise.then(function() {
       var bounds = L.geoJson($scope.feature.data.geojson).getBounds();
       leafletData.getMap().then(function(map) {
         map.fitBounds(bounds);
-
       });
 
       angular.extend($scope, {
         geojson: {
           data: $scope.feature.data.geojson,
           style: sauService.mapConfig.defaultStyle,
-          resetStyleOnMouseout: true
         },
       });
     });
