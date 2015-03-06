a6ad8a1e78b467966001a90e64ad71cd33774b8b
Eric Smith <motionsmith@gmail.com>
2015-03-06 14:50:53 -0800

    Fixed: 1.2M in chart rounds to 1M
    Fixed a bug that rounded the millions labels to much in charts that use
    the “significant digits” filter.

--- app/scripts/filters/significantdigits.js
+++ app/scripts/filters/significantdigits.js
@@ -15,13 +15,7 @@ angular.module('sauWebApp')
     return function (input) {
     	/* TODO We need more specifics about how display numbers before AND after the decimal for each order of magnitude.
     	This is just our first guess. */
-    	input = Number(input);
-    	//How many digits to display.
-    	var digits = 3;
-    	//Get rid of trailing zeroes on large numbers.
-    	if (input % 1000 === 0) { digits = 1; }
-    	else if (input % 100 === 0) { digits = 2; }
     	//This adds a 'sign' to the end to specify the order of magnitude and keep precision to x digits. e.g. 382k
-    	return d3.format(',.' + digits + 's')(input);
+    	return d3.format(',.3s')(Number(input));
     };
   });
