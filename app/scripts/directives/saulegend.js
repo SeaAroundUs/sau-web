6149a269fb2c7cc20561e152e4cc804ee9868013
Eric Smith <motionsmith@gmail.com>
2015-03-05 16:39:10 -0800

    adding template for sau legend directive.

---
+++ app/scripts/directives/saulegend.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name sauWebApp.directive:sauLegend
+ * @description
+ * # sauLegend
+ */
+angular.module('sauWebApp')
+  .directive('sauLegend', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the sauLegend directive');
+      }
+    };
+  });
