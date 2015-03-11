aa8b2fc6f1333c3edc0f8de40fec2c4b5e3f6f19
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-03-11 12:37:13 -0700

    resolved estuary title issue [SAU-283]

--- app/scripts/filters/significantdigits.js
+++ app/scripts/filters/significantdigits.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global d3 */
+
 /**
  * @ngdoc filter
  * @name sauWebApp.filter:significantDigits
@@ -9,7 +11,7 @@
  * Formats chart numbers to the spec.
  * Rounds the number by the magnitude specified, to one decimal.
  * If magnitude is three, then the number is devided by 1000 (three zeros), with one trailing decimal if necessary.
- * 
+ *
  * //Example, with a magnitude of 3.
  *      //123456.789 > 123.4
  *      //800000 > 800
@@ -19,7 +21,7 @@
 angular.module('sauWebApp')
   .filter('significantDigits', function () {
     return function (input, magnitude) {
-        
+
         //Inline examples are with a magnitude = 3.
     	var n = Number(input) / (Math.pow(10, magnitude - 1)); //Takes 123456.789 and turns it into 1234.56789
     	n = Math.round(n) / 10; //Takes 1234.56789 and turns it into 123.4;
