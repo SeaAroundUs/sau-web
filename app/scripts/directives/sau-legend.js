de5c7db25fb89e5a8b83e82a56e551d3abc366ec
Eric Smith <motionsmith@gmail.com>
2015-03-12 15:26:14 -0700

    Test commit.

--- app/scripts/directives/sau-legend.js
+++ app/scripts/directives/sau-legend.js
@@ -4,7 +4,7 @@
  * @ngdoc directive
  * @name sauWebApp.directive:sauLegend
  * @description
- * # A directive to represent the legends that appear below the inset map on the region detail screens.
+ * # - A directive to represent the legends that appear below the inset map on the region detail screens.
  */
 angular.module('sauWebApp')
   .directive('sauLegend', function () {
