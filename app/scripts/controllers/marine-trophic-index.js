ee8f8fa0fb911c3b40be649a636ce4ff2da797d5
Robert Reis <T-RobertR@vulcan.com>
2015-03-27 10:41:04 -0700

    [SAU-591] MTI species list

--- app/scripts/controllers/marine-trophic-index.js
+++ app/scripts/controllers/marine-trophic-index.js
@@ -3,7 +3,7 @@
 /* global angular */
 
 angular.module('sauWebApp')
-  .controller('MarineTrophicIndexCtrl', function ($scope, $routeParams, $modal, sauAPI, region) {
+  .controller('MarineTrophicIndexCtrl', function ($scope, $routeParams, $modal, $http, sauAPI, region) {
 
     $scope.years = [];
     $scope.regionType = region;
@@ -29,15 +29,18 @@ angular.module('sauWebApp')
 
     $scope.region = sauAPI.Region.get({region: region, region_id: id});
 
-    var data = sauAPI.MarineTrophicIndexData.get({region: region, region_id: id}, function() {
+    var species = sauAPI.ExploitedOrganismsData.get({region: region, region_id: id}, function() {
+      $scope.speciesList = species.data;
+    });
 
+    var displayCharts = function(data) {
       $scope.data = data.data;
       angular.forEach($scope.data, function(time_series) {
-          var nullFilteredData = time_series.values.filter(function(x) {
-            return x[1];
-          });
-          time_series.values = nullFilteredData;
-          $scope[time_series.key] = [time_series];
+        var nullFilteredData = time_series.values.filter(function(x) {
+          return x[1];
+        });
+        time_series.values = nullFilteredData;
+        $scope[time_series.key] = [time_series];
       });
 
       angular.forEach($scope.data[0].values, function(xy) {
@@ -45,6 +48,28 @@ angular.module('sauWebApp')
           $scope.years.push(xy[0]);
         }
       });
+    };
 
-    });
+    sauAPI.MarineTrophicIndexData.get({region: region, region_id: id}, displayCharts);
+
+    // compute data with exclusions from species table
+    $scope.compute = function() {
+      var url = sauAPI.apiURL + region + '/marine-trophic-index/?region_id=' + id;
+
+      angular.forEach($scope.speciesList, function(species) {
+        if (species.excluded) {
+          url += '&exclude=' + species.taxon_key;
+        }
+      });
+
+      $http.get(url).success(displayCharts);
+    };
+
+    // select/deselect all exclusion checkboxes on species table
+    $scope.setAllExclusions = function(excluded) {
+      angular.forEach($scope.speciesList, function(species) {
+        species.excluded = excluded;
+      });
+    };
+
   });
