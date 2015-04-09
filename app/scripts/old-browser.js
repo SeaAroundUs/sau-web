16feb9011e8b5a713d5c13782779d956c069290f
Robert Reis <T-RobertR@vulcan.com>
2015-04-09 13:58:30 -0700

    [SAU-714] solution for slow bandwidth

--- app/scripts/old-browser.js
+++ app/scripts/old-browser.js
@@ -1,23 +1,25 @@
+'use strict';
+/* global MobileDetect */
 (function() {
-  'use strict';
+  try {
+    var cookies = {};
+    var keyVals = document.cookie.split('; ');
+    var cookie;
+    var md;
+    var modernBrowser;
 
-  /* global MobileDetect */
+    for (var i = 0; i < keyVals.length; i++) {
+      cookie = keyVals[i].split('=');
+      cookies[cookie[0]] = cookie[1];
+    }
 
-  var cookies = {};
-  var keyVals = document.cookie.split('; ');
-  var cookie;
-  var md;
-  var modernBrowser;
+    md = new MobileDetect(window.navigator.userAgent);
+    modernBrowser = md.match('chrome|firefox') || md.version('IE') >= 9;
 
-  for(var i = 0; i < keyVals.length; i++) {
-    cookie = keyVals[i].split('=');
-    cookies[cookie[0]] = cookie[1];
-  }
-
-  md = new MobileDetect(window.navigator.userAgent);
-  modernBrowser = md.match('chrome|firefox') || md.version('IE') >= 9;
-
-  if (!modernBrowser && cookies.ignoreOldBrowser !== '1' && window.location.pathname !== '/simple-site.php') {
+    if (!modernBrowser && cookies.ignoreOldBrowser !== '1' && window.location.pathname !== '/simple-site.php') {
+      window.location.pathname = '/simple-site.php';
+    }
+  } catch(e) {
     window.location.pathname = '/simple-site.php';
   }
 })();
