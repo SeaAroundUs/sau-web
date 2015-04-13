ce7330e5640325148e1059ae2c09b32fa2e0b393
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-04-13 12:54:22 -0700

    Handled taxon 404

--- app/scripts/controllers/key-info-on-taxon.js
+++ app/scripts/controllers/key-info-on-taxon.js
@@ -2,7 +2,14 @@
 
 angular.module('sauWebApp').controller('KeyInfoOnTaxonCtrl',
   function ($scope, sauAPI, $routeParams) {
-    sauAPI.Taxon.get({taxon_key: $routeParams.taxon}, function(result) {
-      $scope.taxon = result.data;
-    });
+    sauAPI.Taxon.get({taxon_key: $routeParams.taxon},
+        function(result) {
+            $scope.taxon = result.data;
+        },
+        function(error) {
+            $scope.taxon = {
+                error: error,
+                key: $routeParams.taxon
+            };
+        });
   });
