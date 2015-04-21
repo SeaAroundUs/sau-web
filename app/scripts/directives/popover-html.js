b95b614caa7d98e72a865b309cd2de25bb668eab
Robert Reis <T-RobertR@vulcan.com>
2015-04-21 15:08:10 -0700

    [SAU-886] add popover that allows HTML

---
+++ app/scripts/directives/popover-html.js
@@ -0,0 +1,28 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('sauWebApp')
+    .run(function($templateCache) {
+      $templateCache.put('template/popover/popover-html-unsafe-popup.html',
+        '<div class="popover {{placement}}" ng-class="{ in: isOpen(), fade: animation() }">\n' +
+        '  <div class="arrow"></div>\n' +
+        '\n' +
+        '  <div class="popover-inner">\n' +
+        '      <h3 class="popover-title" ng-bind="title" ng-show="title"></h3>\n' +
+        '      <div class="popover-content" bind-html-unsafe="content"></div>\n' +
+        '  </div>\n' +
+        '</div>\n'
+      );
+    })
+    .directive('popoverHtmlPopup', function() {
+    return {
+      restrict: 'EA',
+      replace: true,
+      scope: { title: '@', content: '@', placement: '@', animation: '&', isOpen: '&' },
+      templateUrl: 'template/popover/popover-html-unsafe-popup.html'
+    };
+  })
+  .directive('popoverHtml', function ($tooltip) {
+    return $tooltip('popoverHtml', 'popover', 'click');
+  });
+})(angular);
