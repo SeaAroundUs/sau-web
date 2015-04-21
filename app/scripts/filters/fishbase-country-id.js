0cdd97e4335071daab201621314e8565c4702997
Eric Smith <motionsmith@gmail.com>
2015-04-21 16:06:23 -0700

    Adding treaties and conventions link to EEZ governance tab.

---
+++ app/scripts/filters/fishbase-country-id.js
@@ -0,0 +1,13 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('sauWebApp').filter('fishbaseCountryId', function() {
+    return function(input) {
+      if (Number(input) < 100) {
+        return '0' + input.toString();
+      } else {
+        return input.toString();
+      }
+    };
+  });
+})(angular);
