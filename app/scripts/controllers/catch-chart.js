5272120a6db00ba8833e7238ba3491a70d92f34a
Robert Reis <T-RobertR@vulcan.com>
2015-05-11 12:21:51 -0700

    [SAU-960] use home country with host country to order country data

--- app/scripts/controllers/catch-chart.js
+++ app/scripts/controllers/catch-chart.js
@@ -155,6 +155,7 @@ angular.module('sauWebApp').controller('CatchChartCtrl',
       var data = sauAPI.Data.get(data_options, function() {
         var dataHash;
         var tempData = [];
+        var homeCountry = [];
         var hostCountry = [];
         var otherCountries = [];
         var orderedLabels = {
@@ -183,16 +184,18 @@ angular.module('sauWebApp').controller('CatchChartCtrl',
           });
           $scope.data = tempData;
 
-        // chart ordering host country first
+        // chart ordering home country, then host country
         } else if (data_options.dimension === 'country') {
           data.data.forEach(function(country) {
             if (country.key === data.host_country) {
               hostCountry[0] = country;
+            } else if (country.key === data.home_country) {
+              homeCountry[0] = country;
             } else {
               otherCountries.push(country);
             }
           });
-          $scope.data = hostCountry.concat(otherCountries);
+          $scope.data = homeCountry.concat(hostCountry.concat(otherCountries));
 
         // default chart ordering
         } else {
