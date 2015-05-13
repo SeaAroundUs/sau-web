e1c2b54a4057b3b7e57d1595bdd2e782b27cf030
Eric Smith <motionsmith@gmail.com>
2015-05-13 10:58:32 -0700

    Chart number formatting adds significant digits until it finds a non-zero digit.

--- app/scripts/filters/significantdigits.js
+++ app/scripts/filters/significantdigits.js
@@ -10,7 +10,7 @@
  * # significantDigits
  * Formats chart numbers to the spec.
  * Rounds the number by the magnitude specified, to one decimal.
- * If magnitude is three, then the number is devided by 1000 (three zeros), with one trailing decimal if necessary.
+ * If magnitude is three, then the number is divided by 1000 (three zeros), with one trailing decimal if necessary.
  *
  * //Example, with a magnitude of 3.
  *      //123456.789 > 123.4
@@ -21,9 +21,22 @@
 angular.module('sauWebApp')
   .filter('significantDigits', function () {
     return function (input, magnitude) {
+      for (var i = 1; i < magnitude; i++) {
+        var n1 = Number(input) / Math.pow(10, magnitude - i);
+        var n2 = Math.round(n1) / Math.pow(10, i);
+        if (n2 !== 0) {
+          break;
+        }
+      }
+      return d3.format(',.d')(n2);
+
       //Inline examples are with a magnitude = 3.
-      var n = Number(input) / (Math.pow(10, magnitude - 1)); //Takes 123456.789 and turns it into 1234.56789
-      n = Math.round(n) / 10; //Takes 1234.56789 and turns it into 123.4;
-      return d3.format(',.d')(n); //Adds commas.
+      /*var n1 = Number(input) / (Math.pow(10, magnitude - 1)); //Takes 123456.789 and turns it into 1234.56789
+      var n2 = Math.round(n1) / 10; //Takes 1234.56789 and turns it into 123.4;
+      if (n2 === 0) {
+        n1 = Number(input) / (Math.pow(10, magnitude - 2));
+        n2 = Math.round(n1) / 100;
+      }
+      return d3.format(',.d')(n2); //Adds commas.*/
     };
   });
