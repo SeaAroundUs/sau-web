0cc254001a52f86357f6f6d2fc9d8661be14aeba
Robert Reis <T-RobertR@vulcan.com>
2015-05-19 15:44:33 -0700

    [SAU-508] rearrange biodiversity tab for eez

--- app/scripts/controllers/tab-biodiversity.js
+++ app/scripts/controllers/tab-biodiversity.js
@@ -17,28 +17,23 @@ angular.module('sauWebApp')
           return ('100' + d).slice(-3);
         };
 
-        $scope.fishbaseLinks = {
-          fish: [
-            {label: 'Reef fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'reef'+'&csub_code='},
-            {label: 'Fresh water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'fresh'+'&csub_code='},
-            {label: 'Marine fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'saltwater'+'&csub_code='},
-            {label: 'Pelagic fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'pelagic'+'&csub_code='},
-            {label: 'Deep water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'deepwater'+'&csub_code='},
-            {label: 'Non-fish vertebrates', url: 'http://www.sealifebase.org/speciesgroup/index.php?group=nonfishvertebrates&c_code='+fishbase_id+'&action=list'},
-          ],
+        $scope.fishbaseLinks = [
+          {label: 'Marine fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'saltwater'+'&csub_code='},
+          {label: 'Fresh water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'fresh'+'&csub_code='},
+          {label: 'Pelagic fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'pelagic'+'&csub_code='},
+          {label: 'Reef fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'reef'+'&csub_code='},
+          {label: 'Deep water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'deepwater'+'&csub_code='},
+          {label: 'Threatened fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat=threatened&csub_code='}
+        ];
 
-          threatened: [
-            {label: 'Fish', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat=threatened&csub_code='},
-            {label: 'Non-fish', url: 'http://www.sealifebase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat=threatened&csub_code='},
-          ],
-
-          invertebrates: [
-            {label: 'Crustaceans', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'crustaceans'+'&c_code='+fishbase_id+'&action=list'},
-            {label: 'Mollusks', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'mollusks'+'&c_code='+fishbase_id+'&action=list'},
-            {label: 'Echinoderms', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'echinoderms'+'&c_code='+fishbase_id+'&action=list'},
-            {label: 'Coelenterates', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'coelenterates'+'&c_code='+fishbase_id+'&action=list'}
-          ]
-        };
+        $scope.sealifebaseLinks = [
+          {label: 'Non-fish marine vertebrates', url: 'http://www.sealifebase.org/speciesgroup/index.php?group=nonfishvertebrates&c_code='+fishbase_id+'&action=list'},
+          {label: 'Crustaceans', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'crustaceans'+'&c_code='+fishbase_id+'&action=list'},
+          {label: 'Mollusks', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'mollusks'+'&c_code='+fishbase_id+'&action=list'},
+          {label: 'Echinoderms', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'echinoderms'+'&c_code='+fishbase_id+'&action=list'},
+          {label: 'Coelenterates', url: 'http://www.sealifebase.org/speciesgroup/index.php?group='+'coelenterates'+'&c_code='+fishbase_id+'&action=list'},
+          {label: 'Threatened non-fish organisms', url: 'http://www.sealifebase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat=threatened&csub_code='}
+        ];
 
       } else if ($scope.region.name === 'lme') {
         $scope.fishbaseLink = $scope.feature.data.fishbase_link;
