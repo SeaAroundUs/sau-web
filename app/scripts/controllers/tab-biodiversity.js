61735a071c4c32d0658499695e0e24842bdb1b93
Robert Reis <T-RobertR@vulcan.com>
2015-05-19 15:55:01 -0700

    fix c_code digits for sealifebase
    Conflicts:
    	app/scripts/controllers/tab-biodiversity.js

--- app/scripts/controllers/tab-biodiversity.js
+++ app/scripts/controllers/tab-biodiversity.js
@@ -10,25 +10,20 @@ angular.module('sauWebApp')
       var fishbase_id = null;
 
       if ($scope.region.name === 'eez') {
-        fishbase_id = $scope.feature.data.fishbase_id;
-
-        var padToThree = function(d){
-          // given an integer in the range 0-999, returns a 0-padded string representation
-          return ('100' + d).slice(-3);
-        };
+        fishbase_id = ('100' + $scope.feature.data.fishbase_id).slice(-3);
 
         $scope.fishbaseLinks = {
           fish: [
-            {label: 'Reef fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'reef'+'&csub_code='},
-            {label: 'Fresh water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'fresh'+'&csub_code='},
-            {label: 'Marine fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'saltwater'+'&csub_code='},
-            {label: 'Pelagic fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'pelagic'+'&csub_code='},
-            {label: 'Deep water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat='+'deepwater'+'&csub_code='},
+            {label: 'Reef fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat='+'reef'+'&csub_code='},
+            {label: 'Fresh water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat='+'fresh'+'&csub_code='},
+            {label: 'Marine fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat='+'saltwater'+'&csub_code='},
+            {label: 'Pelagic fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat='+'pelagic'+'&csub_code='},
+            {label: 'Deep water fishes', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat='+'deepwater'+'&csub_code='},
             {label: 'Non-fish vertebrates', url: 'http://www.sealifebase.org/speciesgroup/index.php?group=nonfishvertebrates&c_code='+fishbase_id+'&action=list'},
           ],
 
           threatened: [
-            {label: 'Fish', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+padToThree(fishbase_id)+'&vhabitat=threatened&csub_code='},
+            {label: 'Fish', url: 'http://www.fishbase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat=threatened&csub_code='},
             {label: 'Non-fish', url: 'http://www.sealifebase.org/Country/CountryChecklist.php?c_code='+fishbase_id+'&vhabitat=threatened&csub_code='},
           ],
 
