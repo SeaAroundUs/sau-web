719ddb785b709846f0833e68ef6bbf35c207c332
Robert Reis <T-RobertR@vulcan.com>
2015-06-19 09:45:36 -0700

    [SAU-1195] remove some EEZs from Under Review status

--- app/scripts/directives/under-review.js
+++ app/scripts/directives/under-review.js
@@ -3,13 +3,8 @@
   angular.module('sauWebApp').directive('underReview',
     function() {
       var eezs = [
-        959, // Alaska subarctic (EEZ 959)
-        384, // Ivory Coast (384)
         634, // Qatar (634)
         684, 683, // Saudi Arabia (684 & 683)
-        462, // Maldives (462)
-        570, // Niue (570)
-        842 // Hawaii main (842)
       ];
       var message = '<p class="under-review">** Data under review</p>';
 
