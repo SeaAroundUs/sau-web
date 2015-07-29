adcfcb76aae6149ee9107af397af3ad567484c4c
Robert Reis <T-RobertR@vulcan.com>
2015-07-29 16:00:20 -0700

    [SAU-1425] handle hiding/showing features per environment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,10 +17,13 @@ angular
     'angulartics',
     'angulartics.google.analytics'
   ])
+
+  // don't strip trailing slashes from calculated URLs
   .config(['$resourceProvider', function($resourceProvider) {
-    // Don't strip trailing slashes from calculated URLs
     $resourceProvider.defaults.stripTrailingSlashes = false;
   }])
+
+  // cache settings
   .run(function($http, DSCacheFactory) {
       var cache = new DSCacheFactory('defaultCache', {
         deleteOnExpire: 'aggressive',
@@ -31,14 +34,21 @@ angular
 
       $http.defaults.cache = cache;
     })
-  .config(function ($routeProvider, SAU_CONFIG) {
+
+  // route configurations
+  .config(function ($routeProvider, SAU_CONFIG, togglesProvider) {
+
+    // default and build version routes
     $routeProvider
       .when('/', {
         redirectTo: '/eez'
       })
       .when('/build', {
         template: SAU_CONFIG.buildNumber
-      })
+      });
+
+    // EEZ routes
+    $routeProvider
       .when('/eez', {
         templateUrl: 'views/map.html',
         controller: 'MapCtrl',
@@ -81,7 +91,10 @@ angular
             return sauAPI.Region.get({'region': 'eez', 'region_id': $route.current.params.id}).$promise;
           }
         }
-      })
+      });
+
+    // LME routes
+    $routeProvider
       .when('/lme', {
         templateUrl: 'views/map.html',
         controller: 'MapCtrl',
@@ -107,7 +120,10 @@ angular
         templateUrl: 'views/exploited-organisms.html',
         controller: 'ExploitedOrganismsCtrl',
         resolve: {region: function() {return 'lme';}}
-      })
+      });
+
+    // RFMO routes
+    $routeProvider
       .when('/rfmo', {
         templateUrl: 'views/map.html',
         controller: 'MapCtrl',
@@ -132,57 +148,62 @@ angular
         templateUrl: 'views/stock-status.html',
         controller: 'StockStatusCtrl',
         resolve: {region: function() {return 'rfmo';}}
-      })
-      .when('/highseas', {
-        templateUrl: 'views/map.html',
-        controller: 'MapCtrl',
-        reloadOnSearch: false,
-        resolve: {region: function() {return 'highseas';}}
-      })
-      .when('/highseas/:id', {
-        templateUrl: 'views/region-detail/main.html',
-        controller: 'RegionDetailCtrl',
-        resolve: {region: function() {return 'highseas';}}
-      })
-      .when('/highseas/:id/marine-trophic-index', {
-        templateUrl: 'views/marine-trophic-index.html',
-        controller: 'MarineTrophicIndexCtrl',
-        resolve: {region: function() {return 'highseas';}}
-      })
-      .when('/highseas/:id/stock-status', {
-        templateUrl: 'views/stock-status.html',
-        controller: 'StockStatusCtrl',
-        resolve: {region: function() {return 'highseas';}}
-      })
-      .when('/global', {
-        templateUrl: 'views/region-data/main.html',
-        controller: 'RegionDataCtrl',
-        reloadOnSearch: false,
-        resolve: {region: function() {return 'global';}}
-      })
-      .when('/global/exploited-organisms', {
-        templateUrl: 'views/exploited-organisms.html',
-        controller: 'ExploitedOrganismsCtrl',
-        resolve: {region: function() {return 'global';}}
-      })
-      .when('/global/marine-trophic-index', {
-        templateUrl: 'views/marine-trophic-index.html',
-        controller: 'MarineTrophicIndexCtrl',
-        resolve: {region: function() {return 'global';}}
-      })
-      .when('/global/stock-status', {
-        templateUrl: 'views/stock-status.html',
-        controller: 'StockStatusCtrl',
-        resolve: {region: function() {return 'global';}}
-      })
-      .when('/taxa/:taxon', {
-        templateUrl: 'views/key-info-on-taxon.html',
-        controller: 'KeyInfoOnTaxonCtrl'
-      })
-      .when('/country/:id', {
-        templateUrl: 'views/country-profile.html',
-        controller: 'CountryProfileCtrl'
-      })
+      });
+
+    // High Seas routes
+    if (togglesProvider.$get().isEnabled('highseas')) {
+      $routeProvider
+        .when('/highseas', {
+          templateUrl: 'views/map.html',
+          controller: 'MapCtrl',
+          reloadOnSearch: false,
+          resolve: {region: function() {return 'highseas';}}
+        })
+        .when('/highseas/:id', {
+          templateUrl: 'views/region-detail/main.html',
+          controller: 'RegionDetailCtrl',
+          resolve: {region: function() {return 'highseas';}}
+        })
+        .when('/highseas/:id/marine-trophic-index', {
+          templateUrl: 'views/marine-trophic-index.html',
+          controller: 'MarineTrophicIndexCtrl',
+          resolve: {region: function() {return 'highseas';}}
+        })
+        .when('/highseas/:id/stock-status', {
+          templateUrl: 'views/stock-status.html',
+          controller: 'StockStatusCtrl',
+          resolve: {region: function() {return 'highseas';}}
+        });
+    }
+
+    // Global routes
+    if (togglesProvider.$get().isEnabled('global')) {
+      $routeProvider
+        .when('/global', {
+          templateUrl: 'views/region-data/main.html',
+          controller: 'RegionDataCtrl',
+          reloadOnSearch: false,
+          resolve: {region: function() {return 'global';}}
+        })
+        .when('/global/exploited-organisms', {
+          templateUrl: 'views/exploited-organisms.html',
+          controller: 'ExploitedOrganismsCtrl',
+          resolve: {region: function() {return 'global';}}
+        })
+        .when('/global/marine-trophic-index', {
+          templateUrl: 'views/marine-trophic-index.html',
+          controller: 'MarineTrophicIndexCtrl',
+          resolve: {region: function() {return 'global';}}
+        })
+        .when('/global/stock-status', {
+          templateUrl: 'views/stock-status.html',
+          controller: 'StockStatusCtrl',
+          resolve: {region: function() {return 'global';}}
+        });
+    }
+
+    // mariculture routes
+    $routeProvider
       .when('/mariculture', {
         templateUrl: 'views/mariculture.html',
         controller: 'MaricultureCtrl'
@@ -191,6 +212,17 @@ angular
         templateUrl: 'views/region-detail/main.html',
         controller: 'RegionDetailCtrl',
         resolve: {region: function() {return 'mariculture';}}
+      });
+
+    // non-regional routes
+    $routeProvider
+      .when('/taxa/:taxon', {
+        templateUrl: 'views/key-info-on-taxon.html',
+        controller: 'KeyInfoOnTaxonCtrl'
+      })
+      .when('/country/:id', {
+        templateUrl: 'views/country-profile.html',
+        controller: 'CountryProfileCtrl'
       })
       .when('/subsidy/:id', {
         templateUrl: 'views/subsidy.html',
@@ -211,7 +243,10 @@ angular
       .when('/expedition', {
         templateUrl: 'views/expedition.html',
         controller: 'ExpeditionCtrl'
-      })
+      });
+
+    // advanced search routes
+    $routeProvider
       .when('/search', {
         templateUrl: 'views/advanced-search/advanced-search.html',
         controller: 'AdvancedSearchCtrl'
@@ -221,34 +256,41 @@ angular
         controller: 'RegionDetailCtrl',
         reloadOnSearch: false,
         resolve: {region: function() {return 'multi';}}
-      })
-      //RE-ENABLE WHEN IMPLEMENTING AUTH
-      /*.when('/signup/', {
-        templateUrl: 'views/auth/signup.html',
-        controller: 'SignUpCtrl',
-      })
-      .when('/activate/', {
-        templateUrl: 'views/auth/activate-request.html',
-        controller: 'ActivateRequestCtrl'
-      })
-      .when('/activate/:code', {
-        templateUrl: 'views/auth/activate.html',
-        controller: 'ActivateCtrl'
-      })
-      .when('/me', {
-        templateUrl: 'views/auth/account-settings.html',
-        controller: 'AccountSettingsCtrl'
-      })
-      .when('/password-reset', {
-        templateUrl: 'views/auth/password-reset.html',
-        controller: 'PasswordResetCtrl'
-      })
-      .when('/new-password/:code', {
-        templateUrl: 'views/auth/new-password.html',
-        controller: 'NewPasswordCtrl'
-      })*/
+      });
+
+    // user auth routes
+    if (togglesProvider.$get().isEnabled('auth')) {
+      $routeProvider
+        .when('/signup/', {
+          templateUrl: 'views/auth/signup.html',
+          controller: 'SignUpCtrl'
+        })
+        .when('/activate/', {
+          templateUrl: 'views/auth/activate-request.html',
+          controller: 'ActivateRequestCtrl'
+        })
+        .when('/activate/:code', {
+          templateUrl: 'views/auth/activate.html',
+          controller: 'ActivateCtrl'
+        })
+        .when('/me', {
+          templateUrl: 'views/auth/account-settings.html',
+          controller: 'AccountSettingsCtrl'
+        })
+        .when('/password-reset', {
+          templateUrl: 'views/auth/password-reset.html',
+          controller: 'PasswordResetCtrl'
+        })
+        .when('/new-password/:code', {
+          templateUrl: 'views/auth/new-password.html',
+          controller: 'NewPasswordCtrl'
+        });
+    }
+
+    // missed route handling
+    $routeProvider
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/' //TODO should this 404?
       });
   })
 
@@ -266,8 +308,11 @@ angular
       }
       return original.apply($location, [path]);
     };
+  }])
 
+  // on logout take user back to root path
+  .run(function($rootScope, $location) {
     $rootScope.$on('logOut', function() {
       $location.path('/').replace();
     });
-  }]);
+  });
---
+++ app/scripts/services/toggles.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('sauWebApp')
+  .provider('toggles', function(SAU_CONFIG) {
+    var disabledOnDev = ['auth'];
+    var disabledOnQA = ['auth'];
+    var disabledOnStageAndProd = ['auth', 'highseas', 'global'];
+
+    var disabled = {
+      dev: disabledOnDev,
+      qa: disabledOnQA,
+      stage: disabledOnStageAndProd,
+      prod: disabledOnStageAndProd
+    };
+
+    var toggleFactory = {
+      isEnabled: function(feature) {
+        return disabled[SAU_CONFIG.env].indexOf(feature) === -1;
+      }
+    };
+
+    return { $get: function() { return toggleFactory; }};
+  });
+
+angular.module('sauWebApp')
+  .directive('toggleHide', function(toggles) {
+    return {
+      link: function(scope, ele, attr) {
+        if (!toggles.isEnabled(attr.toggleHide)) {
+          ele.remove();
+        }
+      },
+      restrict: 'A'
+    };
+  });
