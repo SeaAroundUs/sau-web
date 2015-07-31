2d935832154d416d3dd816d86b5b242bb8b247e1
Robert Reis <T-RobertR@vulcan.com>
2015-07-30 18:15:52 -0700

    [SAU-1097, SAU-1220] center fishing entity chart on country for map

--- app/scripts/directives/region-data/region-data-map.js
+++ app/scripts/directives/region-data/region-data-map.js
@@ -5,26 +5,48 @@
 
 angular.module('sauWebApp')
   .directive('regionDataMap', function() {
-    var controller = function($scope, mapConfig, leafletBoundsHelpers, leafletData) {
+    var controller = function($scope, mapConfig, leafletBoundsHelpers, leafletData, sauAPI) {
       angular.extend($scope, {
         defaults: mapConfig.defaults,
         faoLayers: [],
         maxbounds: mapConfig.worldBounds,
         center: { lat: 0, lng: 0, zoom: 3 },
-        layers: { baselayers: mapConfig.baseLayers }
+        layers: { baselayers: mapConfig.baseLayers },
+        style: mapConfig.defaultStyle
       });
 
       leafletData.getMap('region-data-minimap').then(function(map) {
         L.esri.basemapLayer('Oceans').addTo(map);
         L.esri.basemapLayer('OceansLabels').addTo(map);
       });
+
+      $scope.$watch('region', function(region) {
+        sauAPI.Region.get({ region: region.name, region_id: region.id }, updateGeoJSON);
+      });
+
+      function updateGeoJSON(res) {
+        leafletData.getMap('region-data-minimap').then(function(map) {
+          if (res.data && res.data.geojson) {
+            var f = L.geoJson(res.data.geojson);
+            var bounds = f.getBounds();
+            map.fitBounds(bounds);
+          } else {
+            map.setZoom(1);
+          }
+        });
+      }
     };
 
     return {
       controller: controller,
       link: function(scope, ele) {
+        // don't show on global
         if (scope.region.name === 'global') {
           ele.remove();
+
+        // don't show for multiple ids
+        } else if (!scope.region.id) {
+          ele.hide();
         }
       },
       restrict: 'E',
