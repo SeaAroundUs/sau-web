46b127e830e480fd34d36623167d795357e5668d
Robert Reis <T-RobertR@vulcan.com>
2015-08-10 14:06:39 -0700

    add note to region data for fishing entity

--- app/scripts/services/region-data-disclaimer-content.js
+++ app/scripts/services/region-data-disclaimer-content.js
@@ -3,13 +3,15 @@
 angular.module('sauWebApp')
   .factory('regionDataDisclaimerContent', function() {
     var disclaimers = {
-      global: 'NOTE: the data we present (\'reconstructed data\') combine official reported data and ' +
+      global: '<b>NOTE</b>: The data we present (\'reconstructed data\') combine official reported data and ' +
       'reconstructed estimates of unreported data (including major discards), with reference to ' +
       'individual EEZs. Official reported data are <a popover-html="National data are used for some countries.">' +
       'mainly</a> extracted from the Food and Agriculture Organization of the United Nations (FAO) ' +
       '<a href="http://www.fao.org/fishery/statistics/en" target="_blank">FishStat database</a>. For background ' +
-      'information on reconstruction data used, please see the documentation associated with the individual EEZs.'
+      'information on reconstruction data used, please see the documentation associated with the individual EEZs.',
     };
+
+    disclaimers['fishing-entity'] = disclaimers.global;
 
     return {
       getDisclaimer: function(region) {
