5483fe3bf4f9c90b71150de38d8ac41c3a43d520
Robert Reis <T-RobertR@vulcan.com>
2015-08-11 11:51:14 -0700

    [SAU-1347] update arctic sea no data message for high seas

--- app/scripts/services/sau-chart-utils.js
+++ app/scripts/services/sau-chart-utils.js
@@ -63,7 +63,7 @@ angular.module('sauWebApp')
     //This delivers the hard-coded message to be displayed when there is no data for a region id.
     //Provide the region type and ID, and it will return you there proper message.
     getNoDataMessage: function(regionName, regionId) {
-      if (regionName === 'lme' && regionId === 64) {
+      if ((regionName === 'lme' && regionId === 64) || (regionName === 'highseas' && regionId === 18)) {
         return 'Currently no catches due to ice cover';
       } else {
         return 'No data is available for this selection';
