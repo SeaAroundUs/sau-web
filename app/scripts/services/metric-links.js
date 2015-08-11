3d2830c33e2667f09613c179117fea1cf9737aa5
Robert Reis <T-RobertR@vulcan.com>
2015-08-11 11:43:42 -0700

    [SAU-1463] update coral reefs to tropical coral reefs

--- app/scripts/services/metric-links.js
+++ app/scripts/services/metric-links.js
@@ -6,7 +6,7 @@ angular.module('sauWebApp').factory('metricLinks', function(externalURLs) {
     'LME area': externalURLs.areaDef + '#_Toc421807917',
     'Shelf Area': externalURLs.areaDef + '#_Toc421807905',
     'Inshore Fishing Area (IFA)': externalURLs.areaDef + '#_Toc421807906',
-    'Coral Reefs': externalURLs.areaDef + '#_Toc421807907',
+    'Tropical Coral Reefs': externalURLs.areaDef + '#_Toc421807907',
     'Seamounts': externalURLs.areaDef + '#_Toc421807908',
     'Primary production': externalURLs.areaDef + '#_Toc421807913'
   };
