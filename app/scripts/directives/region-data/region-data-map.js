229e065d7ff03c6e3234950fad3d241acf842f56
Robert Reis <T-RobertR@vulcan.com>
2015-09-08 15:49:18 -0700

    [SAU-1574] enhance map for new layout

--- app/scripts/directives/region-data/region-data-map.js
+++ app/scripts/directives/region-data/region-data-map.js
@@ -5,7 +5,12 @@
 
 angular.module('sauWebApp')
   .directive('regionDataMap', function() {
-    var controller = function($scope, mapConfig, leafletBoundsHelpers, leafletData, sauAPI) {
+    var controller = function($scope, $timeout, mapConfig, leafletBoundsHelpers, leafletData, sauAPI,
+                              createDisputedAreaPopup, ga) {
+
+      var isDisputedAreaPopupOpen = false;
+      var selectedRegions;
+
       angular.extend($scope, {
         defaults: mapConfig.defaults,
         faoLayers: [],
@@ -14,44 +19,177 @@ angular.module('sauWebApp')
         layers: { baselayers: mapConfig.baseLayers }
       });
 
+      // add oceans
       leafletData.getMap('region-data-minimap').then(function(map) {
         L.esri.basemapLayer('Oceans').addTo(map);
         L.esri.basemapLayer('OceansLabels').addTo(map);
       });
 
-      $scope.$watch('region', function(region) {
+      $scope.$watch('region.name', drawRegions);
+      $scope.$watch('region.id', centerMap);
+
+      // center map on selected region
+      function centerMap(regionId) {
         sauAPI.Region.get(
-          { region: region.name, region_id: region.id },
-          updateGeoJSON,
+          { region: $scope.region.name, region_id: regionId },
+          function(res) {
+            // keep track of region ids
+            selectedRegions = [regionId];
+
+            // get bounds, record multiple region ids, and zoom map
+            leafletData.getMap('region-data-minimap').then(function(map) {
+              if (res.data && res.data.geojson) {
+                var f = L.geoJson(res.data.geojson, { style: mapConfig.defaultStyle });
+                var bounds = f.getBounds();
+                map.fitBounds(bounds);
+                $scope.disabled = false;
+
+                // handle maps with multiple eezs
+              } else if (res.data && res.data.eezs && res.data.eezs[0].geojson) {
+                var group = new L.featureGroup(res.data.eezs.map(function(eez) {
+                  selectedRegions.push(eez.id);
+                  return L.geoJson(eez.geojson, { style: mapConfig.defaultStyle });
+                }));
+                map.fitBounds(group.getBounds());
+                $scope.disabled = false;
+
+              } else {
+                map.setZoom(1);
+                $scope.disabled = true;
+              }
+
+              // restyle all layers
+              restyleLayers();
+            });
+          },
           function() { $scope.disabled = true; }
         );
-      });
+      }
 
-      //TODO probably needs updating for other geoJSON types (EEZ, IFA, etc)
-      function updateGeoJSON(res) {
+      // add region geojson
+      function drawRegions(name) {
+        sauAPI.Regions.get(
+          { region: name === 'eez-bordering' ? 'eez' : name },
+          function(res) {
+            leafletData.getMap('region-data-minimap').then(function(map) {
+              if (res.data && res.data.features) {
+                L.geoJson(res.data.features, {
+                  style: mapConfig.defaultStyle,
+                  onEachFeature: function(feature, layer) {
+                    layer.on({
+                      click: function(event) {
+                        geojsonClick(feature, event.latlng);
+                      },
+                      mouseover: function(event) {
+                        geojsonMouseover(event, feature, map);
+                      },
+                      mousemove: function(event) {
+                        geojsonMouseover(event, feature, map);
+                      },
+                      mouseout: function(event) {
+                        geojsonMouseout(event, feature, map);
+                      }
+                    });
+                  }
+                }).addTo(map);
+
+                // restyle all layers
+                restyleLayers();
+              }
+            });
+          },
+          function() { $scope.disabled = true; }
+        );
+      }
+
+      // handle geojson click
+      function geojsonClick(feature, latlng) {
+        /* handle clicks on overlapping layers */
         leafletData.getMap('region-data-minimap').then(function(map) {
-          if (res.data && res.data.geojson) {
-            var f = L.geoJson(res.data.geojson, { style: mapConfig.tempCountryStyle });
-            var bounds = f.getBounds();
-            f.addTo(map);
-            map.fitBounds(bounds);
-            $scope.disabled = false;
+          map.closePopup();
 
-          // handle maps with multiple eezs
-          } else if (res.data && res.data.eezs && res.data.eezs[0].geojson) {
-            var group = new L.featureGroup(res.data.eezs.map(function(eez) {
-              var f = L.geoJson(eez.geojson, { style: mapConfig.tempCountryStyle });
-              f.addTo(map);
-              return f;
-            }));
-            map.fitBounds(group.getBounds());
-            $scope.disabled = false;
+          var layers = leafletPip.pointInLayer(latlng, map);
+          var featureLayers = layers.filter(function(l) {
+            //TODO have eez-bordering and country-eez endpoints to return geojson
+            var regionTypeName = $scope.region.name === 'eez-bordering' ? 'eez' : $scope.region.name;
+            // only return layers which have a feature of the current region type
+            return (l.feature && l.feature.properties.region === regionTypeName);
+          });
+
+          if (featureLayers.length > 1) {
+            var popup = createDisputedAreaPopup($scope.region.name, featureLayers);
+
+            ga.sendEvent({
+              category: 'MiniMap Click',
+              action: $scope.region.name.toUpperCase(),
+              label: '(Disputed)'
+            });
+
+            popup.setLatLng(latlng);
+            //I have to open the disputed area popup on a timeout due to a bug that occurs in Chrome on Windows (only).
+            //If a popup closes and then opens in the same tick, then the "popupclose" event doesn't fire.
+            //If the popupclose event doesn't fire, then the hover popup won't close, resulting in both popups being open at the same time.
+            $timeout(function() {
+              map.openPopup(popup);
+              isDisputedAreaPopupOpen = true;
+            });
 
           } else {
-            map.setZoom(1);
-            $scope.disabled = true;
+            ga.sendEvent({
+              category: 'MiniMap Click',
+              action: $scope.region.name.toUpperCase(),
+              label: feature.properties.title
+            });
+
+            $scope.region.id = feature.properties.region_id;
+            $scope.region.ids = [$scope.region.id];
+
+            // restyle all layers
+            restyleLayers();
           }
         });
+      }
+
+      // handle geojson mouseover
+      function geojsonMouseover(event, feature, map) {
+        event.layer.setStyle(mapConfig.highlightStyle);
+        if (!isDisputedAreaPopupOpen) {
+          new L.Rrose({ offset: new L.Point(0, -10, false), closeButton: false, autoPan: false })
+            .setContent(feature.properties.title)
+            .setLatLng(event.latlng)
+            .openOn(map);
+        }
+      }
+
+      // handle geojson mouseout
+      function geojsonMouseout(event, feature, map) {
+        styleLayer(feature, event.layer);
+        if (!isDisputedAreaPopupOpen) {
+          map.closePopup();
+        }
+      }
+
+      // restyle all layers
+      function restyleLayers() {
+        leafletData.getMap('region-data-minimap').then(function(map) {
+          map.eachLayer(function(l) {
+            if (l.feature) {
+              styleLayer(l.feature, l);
+            }
+          });
+        });
+      }
+
+      // re-style single region
+      function styleLayer(feature, layer, style) {
+        if (!layer) {
+          return;
+        }
+        style = style || mapConfig.defaultStyle;
+        if (feature && selectedRegions.indexOf(feature.properties.region_id) !== -1) {
+          style = mapConfig.selectedStyle;
+        }
+        layer.setStyle(style);
       }
     };
 
