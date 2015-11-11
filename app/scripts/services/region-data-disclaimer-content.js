7450399e325750ffd9cea9f83d38c50f2342b7b0
Robert Reis <T-RobertR@vulcan.com>
2015-11-11 10:19:19 -0800

    [LP-26160769] update text on all charts

--- app/scripts/services/region-data-disclaimer-content.js
+++ app/scripts/services/region-data-disclaimer-content.js
@@ -2,35 +2,18 @@
 
 angular.module('sauWebApp')
   .factory('regionDataDisclaimerContent', function() {
-    var disclaimers = {};
-    var baseDisclaimer = '<b>Note</b>: The data we present (\'reconstructed data\') combine official reported data and ' +
+    var masterDisclaimer = '<b>Note</b>: The data we present (\'reconstructed data\') combine official reported data and ' +
       'reconstructed estimates of unreported data (including major discards), with reference to ' +
       'individual EEZs. Official reported data are <a popover-html="National data are used for some countries.">' +
       'mainly</a> extracted from the Food and Agriculture Organization of the United Nations (FAO) ' +
-      '<a href="http://www.fao.org/fishery/statistics/en" target="_blank">FishStat database</a>. ';
-
-    disclaimers.eez = baseDisclaimer +
-      '<b>For background information on reconstruction data, download the .pdf below.</b>';
-
-    disclaimers.rfmo = baseDisclaimer +
-      '<b>For background information on reconstruction data, download the .pdf from relevant ' +
-      'EEZs and the RFMO evaluation report below.</b>';
-
-    disclaimers.fao = baseDisclaimer +
-      '<b>For background information on reconstruction data, download the .pdf associated ' +
-      'with each relevant EEZ.</b>';
-    disclaimers.taxa = disclaimers.fao;
-
-    disclaimers.global = baseDisclaimer + '<b>For background information on reconstruction ' +
-      'data, download the .pdf file for a specific EEZ.</b>';
-    disclaimers.lme = disclaimers.global;
-    disclaimers.highseas = disclaimers.global;
-    disclaimers['fishing-entity'] = disclaimers.global;
-    disclaimers['eez-bordering'] = disclaimers.global;
+      '<a href="http://www.fao.org/fishery/statistics/en" target="_blank">FishStat database</a>. ' +
+      '<b>For background information on reconstruction data, download the .pdf file for the ' +
+      'specific EEZ(s) and also examine our <a href="/catch-reconstruction-and-allocation-methods/">methods</a> ' +
+      'for data and spatial allocation.</b>';
 
     return {
-      getDisclaimer: function(region) {
-        return disclaimers[region] || '';
+      getDisclaimer: function() {
+        return masterDisclaimer;
       }
     };
   });
