088a2afc0c92fd94715314fa27507bed5ad4ab3a
Robert Reis <T-RobertR@vulcan.com>
2016-02-19 11:58:39 -0800

    [SAU-2077] fix mti download issue for reals

--- app/scripts/controllers/marine-trophic-index.js
+++ app/scripts/controllers/marine-trophic-index.js
@@ -96,7 +96,8 @@ angular.module('sauWebApp')
         resolve: {
           dataUrl: function () {
             return sauAPI.apiURL + region + '/marine-trophic-index/?format=csv&region_id=' + id +
-              '&transfer_efficiency=' + $scope.fib.transferEfficiency + '&sub_area_id=' + $routeParams.subRegion;
+              '&transfer_efficiency=' + $scope.fib.transferEfficiency + '&sub_area_id=' +
+              ($routeParams.subRegion || '');
           }
         }
       });
