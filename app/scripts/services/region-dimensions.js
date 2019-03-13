bc91c22f4610d1af35bf8b248dbeb955b85683b5
gtsui091 <Tsui.gordon.9@gmail.com>
2019-03-13 14:53:00 -0700

    fixed white-spaces

--- app/scripts/services/region-dimensions.js
+++ app/scripts/services/region-dimensions.js
@@ -47,7 +47,7 @@ angular.module('sauWebApp')
       showDimensionLimit: false
     };
     //END MOD SORTIZ 11-28-17
-	//BEGIN MOD SORTIZ 10-12-18
+    //BEGIN MOD SORTIZ 10-12-18
     var endUse = {
       label: 'End use',
       value: 'enduse',
@@ -119,9 +119,9 @@ angular.module('sauWebApp')
     //var fishingEntityDimensions = [eez, highseas, taxon, commercialGroup, functionalGroup, gear, fishingSector, catchType, reportingStatus];
     //var taxonDimension = [eez, lme, meow, highseas, fishingCountry, commercialGroup, functionalGroup, gear, fishingSector, catchType, reportingStatus];
     //END MOD SORTIZ 11-28-17
-	
-	//BEGIN MOD SORTIZ 10-12-18
-	var eezDefaultDimensions = [taxon, commercialGroup, functionalGroup, fishingCountry, gear, endUse, fishingSector, catchType, reportingStatus, layer];
+
+    //BEGIN MOD SORTIZ 10-12-18
+    var eezDefaultDimensions = [taxon, commercialGroup, functionalGroup, fishingCountry, gear, endUse, fishingSector, catchType, reportingStatus, layer];
     var defaultDimensions = [taxon, commercialGroup, functionalGroup, fishingCountry, gear, endUse, fishingSector, catchType, reportingStatus, layer];
     var expandedDefaults = [eez, highseas, taxon, commercialGroup, functionalGroup, fishingCountry, gear, endUse, fishingSector, catchType, reportingStatus];
     var eezBorderingDimensions = [eez, taxon, commercialGroup, functionalGroup, fishingCountry, gear, endUse, fishingSector, catchType, reportingStatus];
